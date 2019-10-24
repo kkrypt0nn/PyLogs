739b3b203011e1cb96ff5dbc8e6f9197d8fb6bcf
Krypton <43011723+kkrypt0nn@users.noreply.github.com>
2019-10-24 08:57:57 +0200

    Version 2.0.2
    Fixed pip installation failure

--- CHANGELOGS.md
+++ CHANGELOGS.md
@@ -1,13 +1,22 @@
 # PyLogs Changelogs
+
+##Version 2.0.2
+#### Fixed Features:
+- pip installation
+
 ##Version 2.0.1
 #### Added Features:
 - pip installation now available on [PyPi](https://pypi.org/project/py-logs/)
+
+
 ## Version 2.0.0
 #### Added Features:
 - `__init__` file for easier use of the library
 - `:params` and `:return` in the logging methods for easier understanding
 #### Edited Features:
 - Restructured the whole `pylogs` folder
+
+
 ## Version 1.0.0:
 #### Added Features:
 - Simple working `critical(message, color)`, `error(message, color)`, `warning(message, color)`, `info(message, color)`, `debug(message, color)` and `success(message, color)` methods
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="py-logs",
-    version="2.0.1",
+    version="2.0.2",
     author="Krypton",
     author_email="kkrypt0nn@national.shitposting.agency",
     description="PyLogs is a easy to use python library to let everyone add logs into their python programs easily.",
@@ -13,19 +13,10 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/kkrypt0nn/PyLogs",
     packages=setuptools.find_packages(),
+    python_requires='>=3.7',
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.7',
-    keywords = [
-        'pylogs',
-        'python logging',
-        'logging',
-        'python logs'
-    ],
-    install_requires=[
-        'enum'
     ]
 )
